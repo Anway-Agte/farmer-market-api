12ab5b157b8a1e1e4f2972ec03343d7ec8428798
SteveMaverick <anway.agte@gmail.com>
2021-05-11 16:12:44 +0530

    prettier

--- app.py
+++ app.py
@@ -21,17 +21,13 @@ def index():
 @app.route("/register-user", methods=["POST"])
 def registerUser():
     user = None
-
     try:
         user = request.get_json()
-
     except:
         pass
 
     if user is None:
-
         return ({"err": "There was an error, please try again"}, 400)
-
     else:
         matching_address_user = mongo.db.users.find_one(
             {
@@ -43,54 +39,34 @@ def registerUser():
         )
 
         if matching_address_user:
-
             return {"err": "An account with this address already exists "}
-
         else:
-
             if mongo.db.users.insert_one(user):
-
                 return {"success": "Successfully registered", "id": user["_id"]}
-
             else:
-
                 return {"We're sorry for the inconvinience, please try again."}
 
 
 @app.route("/get-user/", methods=["POST"])
 def get_user():
-
     data = None
-
     try:
-
         data = request.get_json()
-
     except:
-
         pass
 
     if data is None:
-
         return {"err": "Bad request"}
-
     else:
-
         user = None
-
         try:
-
             user = dict(mongo.db.find_one({"_id": data["_id"]}))
-
         except:
             pass
 
         if user is None:
-
             return {"err": "This user does not exist"}
-
         else:
-
             return (user, 200)
 
 
